bb647a785dccbd5b34a912a9b4aaddf7b70782b7
AlexTaG19 <183636842+AlexTaG19@users.noreply.github.com>
2025-02-12 10:11:30 +0000

    sas

--- TiendaInstrumentos/src/Instrumento.java
+++ TiendaInstrumentos/src/Instrumento.java
@@ -2,20 +2,17 @@ package TiendaInstrumentos.src;
 
 import java.time.LocalDate;
 
-public  abstract class Instrumento {
+public abstract class Instrumento {
 
-   private double precio;
-   private String marca;
-   private String modelo;
-   private String dniComprador;
-   private String nombreComprador;
-   private LocalDate fechaCompra;
-   private LocalDate fechaFabricacion;
+    private double precio;
+    private String marca;
+    private String modelo;
+    private String dniComprador;
+    private String nombreComprador;
+    private LocalDate fechaCompra;
+    private LocalDate fechaFabricacion;
 
-     static int sock = 0;
-
-   
-
+    static int sock = 0;
 
     public Instrumento(double precio, String marca, String modelo, String dniComprador, String nombreComprador,
             LocalDate fechaCompra, LocalDate fechaFabricacion) {
@@ -29,52 +26,61 @@ public  abstract class Instrumento {
         sock++;
     }
 
-   public void getInfo(){
-    System.out.println("Marca: " + marca);
-    System.out.println("Modelo: " + modelo);
-    System.out.println("Precio: " + precio);
-    System.out.println("Dni comprador: " + dniComprador);
-    System.out.println("Nombre comprador: " + nombreComprador);
-    System.out.println("Fecha compra: " + fechaCompra);
-    System.out.println("Fecha fabricacion: " + fechaFabricacion);  
-    System.out.println("Cantidad: " + sock);
-    System.out.print(estaDisponible(fechaCompra));
+    public Instrumentodefault(double precio, String marca, String modelo, String dniComprador, String nombreComprador,
+     LocalDate fechaCompra, LocalDate fechaFabricacion) {
+ this.precio = 100;
+this.marca = "royal";
+this.modelo = "royal-23";
+this.dniComprador = "12345678q";
+this.nombreComprador = "Pedro";
+this.fechaCompra = 2022-01-01;
+this.fechaFabricacion = 2021-03-21;
+sock++;
+}
 
-   };
- public  void getAnyos(){
-    int anyos = LocalDate.now().getYear() - fechaFabricacion.getYear();
-    System.out.println("El instrumento se fabrico: " + anyos + " años atrás");
+    public void getInfo() {
+        System.out.println("Marca: " + marca);
+        System.out.println("Modelo: " + modelo);
+        System.out.println("Precio: " + precio);
+        System.out.println("Dni comprador: " + dniComprador);
+        System.out.println("Nombre comprador: " + nombreComprador);
+        System.out.println("Fecha compra: " + fechaCompra);
+        System.out.println("Fecha fabricacion: " + fechaFabricacion);
+        System.out.println("Cantidad: " + sock);
+        System.out.print(estaDisponible(fechaCompra));
 
- };
- public  void estaDisponible( LocalDate fechaCompra){
-    if (fechaCompra == null) {
-        System.out.println("El instrumento no esta disponible");
-    }else {
-        System.out.println("El instrumento esta disponible");
+    };
+
+    public void getAnyos() {
+        int anyos = LocalDate.now().getYear() - fechaFabricacion.getYear();
+        System.out.println("El instrumento se fabrico: " + anyos + " años atrás");
+
+    };
+
+    public void estaDisponible(LocalDate fechaCompra) {
+        if (fechaCompra == null) {
+            System.out.println("El instrumento no esta disponible");
+        } else {
+            System.out.println("El instrumento esta disponible");
+        }
+    };
+
+    public void sonar() {
+        System.out.println("Estoy sonando");
     }
- };
- public void sonar (){
-    System.out.println("Estoy sonando");
- }
 
-
-
-
-      public double getPrecio() {
+    public double getPrecio() {
         return precio;
     }
 
-    
     public void setPrecio(double precio) {
         this.precio = precio;
     }
 
-  
     public String getMarca() {
         return marca;
     }
 
-   
     public void setMarca(String marca) {
         this.marca = marca;
     }
@@ -83,52 +89,44 @@ public  abstract class Instrumento {
         return modelo;
     }
 
-  
     public void setModelo(String modelo) {
         this.modelo = modelo;
     }
 
-   
     public String getDniComprador() {
         return dniComprador;
     }
 
-    
     public void setDniComprador(String dniComprador) {
         this.dniComprador = dniComprador;
     }
 
-      public String getNombreComprador() {
+    public String getNombreComprador() {
         return nombreComprador;
     }
 
-   
     public void setNombreComprador(String nombreComprador) {
         this.nombreComprador = nombreComprador;
     }
 
-    
     public LocalDate getFechaCompra() {
         return fechaCompra;
     }
 
-   
     public void setFechaCompra(LocalDate fechaCompra) {
         this.fechaCompra = fechaCompra;
     }
 
-    
     public LocalDate getFechaFabricacion() {
         return fechaFabricacion;
     }
 
- 
     public void setFechaFabricacion(LocalDate fechaFabricacion) {
         this.fechaFabricacion = fechaFabricacion;
     }
 
-    public class guitarra extends Instrumento{
-    
+    public class guitarra extends Instrumento {
+
         private int cuerdas;
         private int tipo;
 
@@ -144,9 +142,9 @@ public  abstract class Instrumento {
             System.out.println("Estoy sonando una guitarra");
         }
 
-
     }
-    public class bateria extends Instrumento{
+
+    public class bateria extends Instrumento {
 
         private int numeroTambores;
         private int numeroPlatos;
@@ -157,15 +155,11 @@ public  abstract class Instrumento {
             this.numeroTambores = numeroTambores;
             this.numeroPlatos = numeroPlatos;
         }
-        
+
         @Override
         public void sonar() {
             System.out.println("Estoy sonando una bateria");
         }
-        
-    
-    
-
 
     }
 }
